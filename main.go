621912d5090a60e378d924810e88c5d1e5ced2c9
Marcus Ramsden <marcus@bitsden.com>
2024-09-11 19:13:38 +0100

    add state tracking for connected members

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"slices"
+	"sync"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -41,28 +43,62 @@ func main() {
 	dg.Close()
 }
 
+var connectedMembers []string
+var connectedMembersLock sync.RWMutex
+
+func userJoinedServer(s *discordgo.Session, channelID, notifyChannel string, member *discordgo.Member) {
+	connectedMembersLock.RLock()
+	defer connectedMembersLock.RUnlock()
+	if slices.Contains(connectedMembers, member.User.ID) {
+		return
+	}
+
+	connectedMembersLock.Lock()
+	connectedMembers = append(connectedMembers, member.User.ID)
+	connectedMembersLock.Unlock()
+
+	channel, err := s.Channel(channelID)
+	if err != nil {
+		if restErr, ok := err.(*discordgo.RESTError); ok && restErr.Response.StatusCode == 403 {
+			return
+		}
+
+		log.Println("could not resolve channel:", err)
+		return
+	}
+
+	memberName := member.DisplayName()
+	if len(memberName) == 0 || len(channel.Name) == 0 {
+		return
+	}
+
+	_, err = s.ChannelMessageSendEmbed(notifyChannel, &discordgo.MessageEmbed{
+		Description: fmt.Sprintf("%s just joined %s", memberName, channel.Name),
+	})
+	if err != nil {
+		log.Println("failed sending notification to channel:", err)
+	}
+}
+
+func userLeftServer(member *discordgo.Member) {
+	connectedMembersLock.Lock()
+	defer connectedMembersLock.Unlock()
+
+	newConnectedMembers := []string{}
+	for _, connectedMember := range connectedMembers {
+		if connectedMember != member.User.ID {
+			newConnectedMembers = append(newConnectedMembers, member.User.ID)
+		}
+	}
+	connectedMembers = newConnectedMembers
+}
+
 func voiceChannelStateUpdate(notifyChannel string) func(*discordgo.Session, *discordgo.VoiceStateUpdate) {
 	return func(s *discordgo.Session, state *discordgo.VoiceStateUpdate) {
-		channel, err := s.Channel(state.ChannelID)
-		if err != nil {
-			if restErr, ok := err.(*discordgo.RESTError); ok && restErr.Response.StatusCode == 403 {
-				return
-			}
-
-			log.Println("could not resolve channel:", err)
-			return
-		}
-
-		memberName := state.Member.DisplayName()
-		if len(memberName) == 0 || len(channel.Name) == 0 {
-			return
-		}
-
-		_, err = s.ChannelMessageSendEmbed(notifyChannel, &discordgo.MessageEmbed{
-			Description: fmt.Sprintf("%s just joined %s", memberName, channel.Name),
-		})
-		if err != nil {
-			log.Println("failed sending notification to channel:", err)
+		if state.ChannelID == "" {
+			userLeftServer(state.Member)
+		} else {
+			userJoinedServer(s, state.ChannelID, notifyChannel, state.Member)
 		}
 	}
 }
